f525a7b17bc921695130aff89258c2871cdf70eb
Marcos Kalile <marcoskmvasconellos@gmail.com>
2025-10-01 18:37:22 -0300

    Adiciona código de teste de conexão

---
+++ teste_conexao.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+from kortex_api.TCPTransport import TCPTransport
+from kortex_api.RouterClient import RouterClient
+from kortex_api.SessionManager import SessionManager
+
+from kortex_api.autogen.client_stubs.DeviceConfigClientRpc import DeviceConfigClient
+from kortex_api.autogen.client_stubs.BaseClientRpc import BaseClient
+
+from kortex_api.autogen.messages import DeviceConfig_pb2, Session_pb2, Base_pb2
+
+def main():
+    #Import o módulo de conexão
+    
+    import modulo_conexao
+
+    with modulo_conexao.Conexao.create_tcp_connection() as router:
+        
+        device_config = DeviceConfigClient(router)
+        base = BaseClient(router)
+
+        print(device_config.GetDeviceType())
+        print(base.GetArmState())   
+
+if __name__ == "__main__":
+    exit(main())
